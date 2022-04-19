125be26cbe993f2a118f465545d97f2351135bf4
ifetakuro <takurofisayo@gmail.com>
2022-04-19 20:29:40 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/my-todo-app-vue3/",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
